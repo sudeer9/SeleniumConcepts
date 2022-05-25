ca6818e8dee6b3aa963faab26a177257b3dddb44
sudeer9 <43990469+sudeer9@users.noreply.github.com>
2022-05-25 12:38:06 +0530

    Uploading changes

---
+++ src/com/brillio/basics/DemoOrangeLogin.java
@@ -0,0 +1,14 @@
+package com.brillio.basics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoOrangeLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+
+	}
+
+}
